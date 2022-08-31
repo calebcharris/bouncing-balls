506f2acc0374862ac2b687545cf58a5cc32aa9a2
Caleb Harris <chrisisverybusy@outlook.com>
2022-08-31 15:22:57 -0500

    Wanted more balls

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ class Ball {
 
 const balls = []
 
-while (balls.length < 25) {
+while (balls.length < 100) {
     const size = random(10, 20)
     const ball = new Ball(
         // ball position always drawn at leaast one ball width
